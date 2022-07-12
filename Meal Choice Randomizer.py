e61788c39666202b4dd0bb67a4de5eeaf53f10af
MVodopich <108097915+MVodopich@users.noreply.github.com>
2022-07-12 16:57:41 -0700

    Add files via upload

---
+++ Meal Choice Randomizer.py
@@ -0,0 +1,15 @@
+import random
+
+Entree = [input("Entree Choices. Enter your first choice: "), input("Enter your second choice: "),
+input("Enter your third choice: ")]
+Side_1 = [input("Side 1 choices. Enter your first choice: "), input("Enter your second choice: "),
+input("Enter your third choice: ")]
+Side_2 = input("Side 2 Choices. Enter your first choice: "), input("Enter your second choice: "),
+input("Enter your third choice: ")
+print(random.choice(Entree))
+print(random.choice(Side_1))
+print(random.choice(Side_2))
+
+print(random.choice(Entree))
+print(random.choice(Side_1))
+print(random.choice(Side_2))
